ff44b9950a8bb51934ec34e87fc286ba8df6d0cc
ozzIE <ozzie238s@gmail.com>
2016-05-22 23:11:24 -0600

    Cambios a pagos y a base de datos. Se intenta agregar fecha a BD

--- GymHelper/src/GymPackage/MainPage.java
+++ GymHelper/src/GymPackage/MainPage.java
@@ -25,35 +25,11 @@ public class MainPage extends javax.swing.JFrame {
         static Connection conn = null;
         static Statement st = null;
         static ResultSet rs = null;
-        static String ID;
+        
     public MainPage() {
         initComponents();
         this.setResizable(false);
         this.setLocationRelativeTo(getParent());
-        
-
-        jTextField1.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mousePressed(MouseEvent e) {
-                if (jTextField1.getText().equals("Ingresar Numero De Cliente")) {
-                    jTextField1.setText("");
-                }
-                jTextField1.setForeground(Color.black);
-            }
-
-        });
-        jTextField2.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mousePressed(MouseEvent e) {
-                if (jTextField2.getText().equals("Ingresar Numero De Cliente")) {
-                    jTextField2.setText("");
-                }
-                jTextField2.setForeground(Color.black);
-            }
-
-        });
-        
-        
     }
     
     
@@ -75,16 +51,12 @@ public class MainPage extends javax.swing.JFrame {
         jSeparator1 = new javax.swing.JSeparator();
         jPanel2 = new javax.swing.JPanel();
         jSeparator2 = new javax.swing.JSeparator();
-        jLabel3 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
         jButton6 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
         jPanel3 = new javax.swing.JPanel();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
-        jTextField1 = new javax.swing.JTextField();
         jButton4 = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
         jButton7 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -128,18 +100,6 @@ public class MainPage extends javax.swing.JFrame {
 
         getContentPane().add(jPanel1, java.awt.BorderLayout.PAGE_START);
 
-        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
-        jLabel3.setForeground(new java.awt.Color(0, 0, 102));
-        jLabel3.setText("Aqui Podras Registrar Un Pago");
-
-        jTextField2.setForeground(new java.awt.Color(102, 102, 102));
-        jTextField2.setText("Ingresar Numero De Cliente");
-        jTextField2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField2ActionPerformed(evt);
-            }
-        });
-
         jButton6.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         jButton6.setForeground(new java.awt.Color(0, 0, 102));
         jButton6.setText("Realizar Pago");
@@ -163,15 +123,11 @@ public class MainPage extends javax.swing.JFrame {
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jSeparator2, javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addGap(21, 21, 21)
-                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel3)
-                            .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jButton6, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(69, 69, 69)
+                        .addComponent(jButton6, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 169, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 16, Short.MAX_VALUE)))
+                        .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 141, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         jPanel2Layout.setVerticalGroup(
@@ -180,13 +136,9 @@ public class MainPage extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, 2, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addComponent(jLabel3)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(jButton6, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 43, Short.MAX_VALUE)
-                    .addComponent(jButton3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, 43, Short.MAX_VALUE)
+                    .addComponent(jButton6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap(19, Short.MAX_VALUE))
         );
 
@@ -211,14 +163,6 @@ public class MainPage extends javax.swing.JFrame {
             }
         });
 
-        jTextField1.setForeground(new java.awt.Color(102, 102, 102));
-        jTextField1.setText("Ingresar Numero De Cliente");
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
-            }
-        });
-
         jButton4.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         jButton4.setForeground(new java.awt.Color(0, 0, 102));
         jButton4.setText("Verificar Membresia");
@@ -227,10 +171,6 @@ public class MainPage extends javax.swing.JFrame {
                 jButton4ActionPerformed(evt);
             }
         });
-
-        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(0, 0, 102));
-        jLabel1.setText("Ingresar Matricula A Revisar");
 
         jButton7.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         jButton7.setForeground(new java.awt.Color(0, 0, 102));
@@ -246,35 +186,27 @@ public class MainPage extends javax.swing.JFrame {
         jPanel3Layout.setHorizontalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel3Layout.createSequentialGroup()
-                .addContainerGap(32, Short.MAX_VALUE)
-                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 174, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(76, 76, 76)
+                .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 201, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 155, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton7, javax.swing.GroupLayout.PREFERRED_SIZE, 155, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 155, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(26, Short.MAX_VALUE))
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, 155, Short.MAX_VALUE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 155, Short.MAX_VALUE)
+                    .addComponent(jButton7, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(152, Short.MAX_VALUE))
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel3Layout.createSequentialGroup()
-                .addGap(62, 62, 62)
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(26, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
-                .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButton7, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(jPanel3Layout.createSequentialGroup()
+                        .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 61, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jButton7, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGap(4, 4, 4))
-            .addComponent(jButton4, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         getContentPane().add(jPanel3, java.awt.BorderLayout.CENTER);
@@ -288,22 +220,15 @@ public class MainPage extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-           try{
-            Visitas vist = new Visitas();
-           int a= Integer.parseInt(jTextField1.getText());
-           vist.visitas(a);
-            } catch (Exception x) {
-               JOptionPane.showMessageDialog(this,"No se a ingresado una matricula valida");
-        }
            
 
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
-            if (jTextField2.getText().equals("Ingresar Numero De Cliente")||jTextField2.getText().isEmpty() )
-            {JOptionPane.showMessageDialog(this,"No se a ingresado una matricula valida");}
-            else{ Pagos payment = new Pagos();
-            payment.Pago();}
+
+        Pagos payment = new Pagos();
+            payment.Pago();
+           
     }//GEN-LAST:event_jButton6ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
@@ -315,20 +240,11 @@ public class MainPage extends javax.swing.JFrame {
         
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
-    
-    }//GEN-LAST:event_jTextField1ActionPerformed
-
     private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
        
         MostrarCliente show = new MostrarCliente();
            show.setVisible(true);
     }//GEN-LAST:event_jButton7ActionPerformed
-
-    private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
-        MainPage.ID = jTextField2.getText(); 
-      
-    }//GEN-LAST:event_jTextField2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -375,8 +291,6 @@ public class MainPage extends javax.swing.JFrame {
     private javax.swing.JButton jButton4;
     private javax.swing.JButton jButton6;
     private javax.swing.JButton jButton7;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
@@ -384,7 +298,5 @@ public class MainPage extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel3;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JSeparator jSeparator2;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
     // End of variables declaration//GEN-END:variables
 }
--- GymHelper/src/GymPackage/Pagos.java
+++ GymHelper/src/GymPackage/Pagos.java
@@ -11,15 +11,18 @@ import static GymPackage.MainPage.conn;
 import static GymPackage.NewCustomer.rs;
 import static GymPackage.NewCustomer.st;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-
 
 /**
  *
- * @author ozzIE
- * This class will verify if the customer ID has paid yet
+ * @author ozzIE This class will verify if the customer ID has paid yet
  */
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import oracle.net.aso.a;
 import oracle.net.aso.d;
@@ -29,54 +32,102 @@ import oracle.net.aso.g;
 import oracle.net.aso.h;
 import oracle.net.aso.i;
 import oracle.security.o5logon.b;
-public class Pagos{
-    
-    private static java.sql.Date getCurrentDate() {
-    java.util.Date today = new java.util.Date();
-    return new java.sql.Date(today.getTime());
-}
-    
 
-    public void Pago(){
-        
-          int confirm = JOptionPane.showConfirmDialog(null, "Realizar pago de Mensualidad?");
-        
-        if (confirm == 0)
-        {
-          Date date = new Date();
-          String datef = "dd,MMM,yyyy";
-          SimpleDateFormat simple = new SimpleDateFormat(datef);
-          System.out.println(simple.format(date));
-          
-          
-          //String d= "to_date('"+getCurrentDate()+"','yyyy,mm,dd'";
-          
-       /* try{
-          
-            System.out.println(getCurrentDate());
-           
-            System.out.println(getCurrentDate());
-            String script = "INSERT INTO pagos values(?,?,?)";
-            PreparedStatement psta = conn.prepareStatement(script);
-            psta.setInt(1, p);
-            psta.setDate(2, java.sql.Date.valueOf(LocalDate.MIN));
-            psta.setDouble(3, c);
-            psta.execute();
-            psta.close(); 
-           
-        }
-        catch(Exception e)
-        {
-            System.out.println("It didnt work ");
-        }
-        */
-        }
-        
+public class Pagos {
+
+    private static java.sql.Date getCurrentDate() {
+       
+        java.util.Date today = new java.util.Date();
+        return new java.sql.Date(today.getTime());
+
     }
     
     
-   
-  
-    
-    
+
+    public void Pago() {
+        String id;
+
+        try {
+
+            id = JOptionPane.showInputDialog("Ingrese la matricula del cliente que hara el pago");
+            System.out.println("Its " + id);
+            rs = st.executeQuery("SELECT Nombre1_cliente from clientes WHERE Id_cliente = " + id);
+            rs.next();
+            rs.getString(1);
+
+         DateFormat dateFormat = new SimpleDateFormat("dd-MMM-yy");
+         Date date = new Date();
+         System.out.println(dateFormat.format(date)); //2014/08/06 15:59:48
+            
+               
+               
+            String monto = JOptionPane.showInputDialog("De cuanto sera el pago de " + rs.getString(1) + "?");
+
+            rs = st.executeQuery("select count(id_cliente) from pagos where id_cliente = " + id);
+            rs.next();
+            int ID_Pago = rs.getInt(1) + 1;
+
+            String script = "INSERT INTO pagos values(?,?,TO_DATE(?,  'YYYY-MM-DD\"T\"HH24:MI:SS\"Z\"'),?)";
+            PreparedStatement psta = conn.prepareStatement(script);
+            psta.setInt(1, ID_Pago);
+            psta.setFloat(2, Float.parseFloat(monto));
+            psta.setString(3, dateFormat.format(date));
+            psta.setString(4, id);
+            JOptionPane.showMessageDialog(null, "Se realizo el pago exitosamente!");
+            System.out.println(ID_Pago);
+            System.out.println(Float.parseFloat(monto));
+            System.out.println(dateFormat.format(date));
+            System.out.println(id);
+        } catch (Exception x) {
+            JOptionPane.showMessageDialog(null, "error");
+
+        }
+
+        /*
+         if (!MainPage.ID.isEmpty())
+         {
+         String confirm = JOptionPane.showInputDialog("Cuanto desea pagar el cliente?");
+            
+          
+          
+         try {
+                  
+         System.out.println(ID_Pago);
+         } catch (SQLException ex) {
+         Logger.getLogger(Pagos.class.getName()).log(Level.SEVERE, null, ex);
+         }
+               
+         try {
+         String script = ("");
+         PreparedStatement psta = conn.prepareStatement(script); 
+         psta.setString(1, a);
+         } catch (SQLException ex) {
+         Logger.getLogger(Pagos.class.getName()).log(Level.SEVERE, null, ex);
+         }
+          
+         //String d= "to_date('"+getCurrentDate()+"','yyyy,mm,dd'";
+          
+         try{
+          
+         System.out.println(getCurrentDate());
+           
+         System.out.println(getCurrentDate());
+           
+         psta.setDate(2, java.sql.Date.valueOf(LocalDate.MIN));
+         psta.setDouble(3, c);
+         psta.execute();
+         psta.close(); 
+           
+         }
+         catch(Exception e)
+         {
+         System.out.println("It didnt work ");
+         }
+        
+         }
+         else
+         {JOptionPane.showMessageDialog(null,"Por favor indique que cliente realizara el pago");}
+         */
+    }
+
 }
